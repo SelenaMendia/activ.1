84faeaaef6d0a29d923b1b97bc90b8ad7b4a66db
SelenaMendia <103127457+SelenaMendia@users.noreply.github.com>
2022-11-14 22:47:23 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace tp_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            const int CantDias=7;
+            
+            string[] dias = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"};
+
+            for (int i = 0; i < CantDias; i++)
+            {
+                Console.WriteLine("En el elemento {0} es el dia {1}",i,dias[i]);
+            }
+
+            Console.WriteLine("Presione una tecla para terminar...");
+            Console.ReadKey();
+        }
+    }
+}
